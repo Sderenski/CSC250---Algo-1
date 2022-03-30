b1bee42a625e4fd600756823216f500bab6030a8
Stephen Derenski <33239267+Sderenski@users.noreply.github.com>
2022-03-30 15:56:37 -0600

    Working Version for Exact ISO Values

--- Isomorphic Strings/src/edu/neumont/Controller/Controller.java
+++ Isomorphic Strings/src/edu/neumont/Controller/Controller.java
@@ -11,6 +11,8 @@ package edu.neumont.Controller;
         6.) Then add the ISO words to different array lists depending on their values
         7.) Output the values to console and text file.
 
+        I am confused what makes an Isometric value a non Iso string?
+
 
         File Path for Input - TEST:
         C:\Users\sderenski\Downloads\IsomorphInput1.txt
@@ -32,12 +34,13 @@ public class Controller {
     private FileOutputStream output = null;
     private ArrayList<String> fileWords = new ArrayList<>();
     private HashMap<String, ArrayList<String>> exactISO = new HashMap<>();
+    private HashMap<String, ArrayList<String>> looseISO = new HashMap<>();
 
     // Main Run function
     public void run(){
         fileExists();
         for (var word : fileWords){
-            hashAdding(word.toCharArray(), word);
+            exactHashAdding(word.toCharArray(), word);
             // iterate through the chars in the word
             // checking if the char is new or repeated
         }
@@ -51,7 +54,7 @@ public class Controller {
         while(!quit){
             try{
                 fileString = Console.getString("Enter in File Path: ");
-                input = new BufferedReader(new FileReader(fileString));
+                input = new BufferedReader(new FileReader("C:\\Users\\sderenski\\Downloads\\IsomorphInput1.txt"));
                 String line = input.readLine();
                 while(line != null){
                     fileWords.add(line);
@@ -68,30 +71,33 @@ public class Controller {
     // function to check if the letters have been used before or not
     // Find a way to iterate through the hashmap and if it isn't there
     // add it and create a new value for it
-
-    // Now create a string of the values. Sort them.
-    private void hashAdding(char[] wordChar, String word){
+    private void exactHashAdding(char[] wordChar, String word){
+        int offSet = 0;
         ArrayList<Character> letters = new ArrayList<>();
-        HashMap<Integer, ArrayList<Character>> charMap = new HashMap<>();
+        ArrayList<Integer> isoValueInt = new ArrayList<>();
         for (int i = 0; i < wordChar.length; i++) {
             int count = compareLetter(letters, wordChar, i);
             if (count >= 0){
-                charMap.get(count).add(wordChar[i]);
+                isoValueInt.add(count);
+                offSet++;
             }else {
-                charMap.put(i, new ArrayList<>());
-                charMap.get(i).add(wordChar[i]);
                 letters.add(wordChar[i]);
+                isoValueInt.add(i - offSet);
             }
         }
-        exactISOAssign(charMap, word);
+        String key = stringBuilder(isoValueInt);
+        if (!checkKeys(key)) exactISO.put(key, new ArrayList<>());
+        exactISO.get(key).add(word);
+//        view.displayExact(exactISO);
+        //exactISOAssign(charMap, word);
     }
+
 
     // Tries to find if the letter has been found or not in the list, if so then return the key number
     private int compareLetter(ArrayList<Character> letters, char[] wordChar, int count){
         int keyValue = 0;
         for (char letter : letters){
             if (letter == wordChar[count]){
-                System.out.println(letter);
                 return keyValue;
             }
             keyValue++;
@@ -108,26 +114,8 @@ public class Controller {
         return false;
     }
 
-    private void exactISOAssign(HashMap<Integer, ArrayList<Character>> charMap, String word){
-        ArrayList<String> hashKey = new ArrayList<>();
-        Iterator charIter = charMap.entrySet().iterator();
-        while (charIter.hasNext()) {
-            Map.Entry charElem = (Map.Entry) charIter.next();
-            hashKey.add(String.valueOf((int) charElem.getKey()));
-        }
-        String key = stringBuilder(hashKey);
-
-        // Create the key with the arraylist added to it
-        // then Use the key to add the word to it using add....
-        // exactISO[key].add(word);
-        if(!checkKeys(key)){
-            exactISO.put(key, new ArrayList<String>());
-        }
-        exactISO.get(key).add(word);
-    }
-
     // Appending the key together to build a string
-    private String stringBuilder(ArrayList<String> strings){
+    private String stringBuilder(ArrayList<Integer> strings){
         String fin = "";
         for (var str : strings){
             fin += str;
