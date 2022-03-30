6bbc74229ea2a63650642b70f26d143a9b75731a
Stephen Derenski <33239267+Sderenski@users.noreply.github.com>
2022-03-30 10:57:15 -0600

    Added most of the logic to assigning the ISO values to string inputs.

--- Isomorphic Strings/src/edu/neumont/Controller/Controller.java
+++ Isomorphic Strings/src/edu/neumont/Controller/Controller.java
@@ -12,46 +12,52 @@ package edu.neumont.Controller;
         7.) Output the values to console and text file.
 
 
-        File Path for Input:
+        File Path for Input - TEST:
         C:\Users\sderenski\Downloads\IsomorphInput1.txt
      */
 
+import edu.neumont.View.Display;
 import example.lib.Console;
 import example.lib.IOStream;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.FileReader;
+import java.util.*;
 
 public class Controller {
+    private Display view = new Display();
     private String fileString;
-    private FileInputStream input = null;
+    private BufferedReader input = null;
     private FileOutputStream output = null;
-    private ArrayList<String> fileWords = new ArrayList<String>();
-    private HashMap<Integer, String> exactISO = new HashMap<Integer, String>();
+    private ArrayList<String> fileWords = new ArrayList<>();
+    private HashMap<String, ArrayList<String>> exactISO = new HashMap<>();
 
+    // Main Run function
     public void run(){
         fileExists();
         for (var word : fileWords){
-            hashAdding(word.toCharArray());
+            hashAdding(word.toCharArray(), word);
             // iterate through the chars in the word
             // checking if the char is new or repeated
         }
-
+        view.displayExact(exactISO);
 
     }
 
-
+    // Checking to see if the value exists or not
     private void fileExists(){
         boolean quit = false;
         while(!quit){
             try{
                 fileString = Console.getString("Enter in File Path: ");
-                input = new FileInputStream(fileString);
-                fileWords.add(IOStream.readToString(input));
+                input = new BufferedReader(new FileReader(fileString));
+                String line = input.readLine();
+                while(line != null){
+                    fileWords.add(line);
+                    line = input.readLine();
+                }
+                input.close();
                 quit = true;
             }catch(Exception e){
                 System.out.println("That file doesn't exist");
@@ -62,37 +68,70 @@ public class Controller {
     // function to check if the letters have been used before or not
     // Find a way to iterate through the hashmap and if it isn't there
     // add it and create a new value for it
-    private void hashAdding(char[] wordChar){
-        int counter = 0;
-        ArrayList<Character> letters = new ArrayList<Character>();
-        HashMap<Integer, Character> charMap = new HashMap<Integer, Character>();
+
+    // Now create a string of the values. Sort them.
+    private void hashAdding(char[] wordChar, String word){
+        ArrayList<Character> letters = new ArrayList<>();
+        HashMap<Integer, ArrayList<Character>> charMap = new HashMap<>();
         for (int i = 0; i < wordChar.length; i++) {
-            if (compareLetter(letters, wordChar, i)){
+            int count = compareLetter(letters, wordChar, i);
+            if (count >= 0){
+                charMap.get(count).add(wordChar[i]);
+            }else {
+                charMap.put(i, new ArrayList<>());
+                charMap.get(i).add(wordChar[i]);
                 letters.add(wordChar[i]);
-                charMap.put(counter, wordChar[i]);
-            } else{
-                assigningHash(wordChar, i, charMap);
             }
         }
+        exactISOAssign(charMap, word);
     }
 
-    public boolean compareLetter(ArrayList<Character> letters, char[] wordChar, int count){
+    // Tries to find if the letter has been found or not in the list, if so then return the key number
+    private int compareLetter(ArrayList<Character> letters, char[] wordChar, int count){
+        int keyValue = 0;
         for (char letter : letters){
-            if (letter != wordChar[count]){
+            if (letter == wordChar[count]){
+                System.out.println(letter);
+                return keyValue;
+            }
+            keyValue++;
+        }
+        return -1;
+    }
+
+    private boolean checkKeys(String key){
+        for (String i : exactISO.keySet()){
+            if (i.equals(key)){
                 return true;
             }
         }
         return false;
     }
 
-    public void assigningHash(char[] wordChar, int count, HashMap<Integer, Character> charMap){
+    private void exactISOAssign(HashMap<Integer, ArrayList<Character>> charMap, String word){
+        ArrayList<String> hashKey = new ArrayList<>();
         Iterator charIter = charMap.entrySet().iterator();
-        while (charIter.hasNext()){
-            Map.Entry charElem = (Map.Entry)charIter.next();
-            if ((char)charElem.getValue() == wordChar[count]){
-                charMap.put((int)charElem.getKey(), (char)charElem.getValue());
-                break;
-            }
+        while (charIter.hasNext()) {
+            Map.Entry charElem = (Map.Entry) charIter.next();
+            hashKey.add(String.valueOf((int) charElem.getKey()));
         }
+        String key = stringBuilder(hashKey);
+
+        // Create the key with the arraylist added to it
+        // then Use the key to add the word to it using add....
+        // exactISO[key].add(word);
+        if(!checkKeys(key)){
+            exactISO.put(key, new ArrayList<String>());
+        }
+        exactISO.get(key).add(word);
+    }
+
+    // Appending the key together to build a string
+    private String stringBuilder(ArrayList<String> strings){
+        String fin = "";
+        for (var str : strings){
+            fin += str;
+        }
+        return fin;
     }
 }
--- Isomorphic Strings/src/edu/neumont/View/Display.java
+++ Isomorphic Strings/src/edu/neumont/View/Display.java
@@ -1,5 +1,18 @@
 package edu.neumont.View;
 
-public class Display {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class Display {
+    // To display the outputs, I need to iterate through the hashmap
+    // show the keys first and then the values
+    public void displayExact(HashMap<String, ArrayList<String>> exactISO){
+        for (var key : exactISO.keySet()){
+            System.out.print(key + ": ");
+            for (var valueISO : exactISO.get(key)){
+                System.out.print(valueISO + " ");
+            }
+            System.out.println();
+        }
+    }
 }
